528c15a47dd7312624ca8418b7393073eefd9257
Simoris <carmot-gc@web.de>
2019-01-22 11:29:35 +0100

    Tested and debugged parts of Device and Effect. Added temporary main in Effect for this purpose.

--- src/c4l/applet/device/Effect.java
+++ src/c4l/applet/device/Effect.java
@@ -30,7 +30,20 @@ public class Effect {
 		state = (state + speed) % Constants.EFFECTRANGE;
 	}
 	public int[] apply(int[] in) {
+		System.out.println("Applying Effect." + String.valueOf(size));
 		return in; //TODO
 	}
 	
+	
+	//Test-Main
+	public static void main(String[] args) {
+		int[] perm = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		Device d = new Device(perm);
+		d.setInputs(perm);
+		Effect e = new Effect(0, 0, 0);
+		Effect e1 = new Effect(1, 0, 0);
+		d.addEffect(e);
+		d.addEffect(e1);
+		d.getOutput();
+	}	
 }
