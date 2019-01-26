f2b3f5e7736d597d055cd8df6c37eb984b8a9fd6
Andre Soltau <Modellbauer555@web.de>
2019-01-26 17:33:26 +0100

    Zum genereien von testObjeckten

--- src/c4l/applet/main/TestObjeckte.java
+++ src/c4l/applet/main/TestObjeckte.java
@@ -6,16 +6,16 @@ import c4l.applet.main.*;
 public class TestObjeckte {
 	
 	/** Device array zum testen **/
-	private Device[] Devices = new Device[Constants.ALL_DEVICES];
-	private int[] perm = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}; // endweder nul oder 16 ist zuviel 
+	private static Device[] Devices = new Device[Constants.ALL_DEVICES];
+	private static int[] perm = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; // endweder nul oder 16 ist zuviel 
 	
 
-	public Device[] generateDevices() {
-		for(byte i = 0; i <= Constants.ALL_DEVICES ;i++) {
-			Device device = new Device(perm,1+(i*16) );
-			device.setInput(1, 200); // Sollte noch durch Random erweitert wertden 
-			device.setInput(2, 100);
-			this.Devices[i] = device;
+	public static Device[] generateDevices() {
+		for(byte i = 0; i < Constants.ALL_DEVICES ;i++) {
+			Device device = new Device(perm,(i*16) );
+			device.setInput(0, 200); // Sollte noch durch Random erweitert wertden 
+			device.setInput(1, 100);
+			Devices[i] = device;
 		}
 		
 		return	Devices;	
