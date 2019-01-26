e83ada169c7afb46691c268178cb01df4b9e4577
Andre Soltau <Modellbauer555@web.de>
2019-01-26 14:38:27 +0100

    .

--- src/c4l/applet/main/TestObjeckte.java
+++ src/c4l/applet/main/TestObjeckte.java
@@ -1,16 +1,17 @@
 package c4l.applet.main;
 
 import c4l.applet.device.*;
+import c4l.applet.main.*;
 
 public class TestObjeckte {
 	
 	/** Device array zum testen **/
-	private Device[] Devices = new Device[30];
+	private Device[] Devices = new Device[Constants.ALL_DEVICES];
 	private int[] perm = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}; // endweder nul oder 16 ist zuviel 
 	
 
 	public Device[] generateDevices() {
-		for(byte i = 0; i <= 30 ;i++) {
+		for(byte i = 0; i <= Constants.ALL_DEVICES ;i++) {
 			Device device = new Device(perm);
 			device.setInput(1, 200); // Sollte noch durch Random erweitert wertden 
 			device.setInput(2, 100);
