9be0031154880aba87e7909396373aad0cc9d673
Andre Soltau <Modellbauer555@web.de>
2019-01-26 17:35:26 +0100

    anpassung der constants

--- src/c4l/applet/main/Constants.java
+++ src/c4l/applet/main/Constants.java
@@ -21,8 +21,8 @@ public class Constants {
 	
 	// Output constants
 	/** constanten für den Output handling mit entec **/
-	public final static int  MAXCHANNEL = 512;
-	public final static int  MINCHANNEL = 1;
+	public final static int  MAXCHANNEL = 511;
+	public final static int  MINCHANNEL = 0;
 	public final static int  MAXVALUE = 255;
 	public final static int  MINVALUE = 0;
 
