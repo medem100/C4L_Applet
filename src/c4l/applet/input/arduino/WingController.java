2104b12f28aba16705ff135a40bb1edc7084f524
Simoris <carmot-gc@web.de>
2019-02-20 00:10:26 +0100

    Minor Improvements
    Added some additional comments for variables in WingController. Moved
    Constants from ProFirmata and WingController-hardcode into
    arduino.properties.

--- src/c4l/applet/input/arduino/WingController.java
+++ src/c4l/applet/input/arduino/WingController.java
@@ -24,30 +24,40 @@ public class WingController {
 	private Arduino arduino;	
 	
 	//Faders
+	/** Buffer for normal fader bank */
 	private int[] faders;
-	int[] bFaders;
+	/** Buffer for multiplexed faders */
+	private int[] bFaders;
 	
 	//Device Choice
-	private boolean[] lastState; //last state of DeviceTransmission
-	private boolean[] activityChanged; //whether there was a change to lastState
-	private boolean[] lastSelection; //last transmitted DeviceSelection
+	/** Last transmitted state of for device selection from hardware (transmitted in DeviceTransmission).
+	 * This is independent (and may differ) from DeviceSelection. Input-class should change DeviceSelection on flanks of DeviceTransmission */
+	private boolean[] lastState;
+	/** Whether there was a change to lastState */
+	private boolean[] activityChanged;
+	/** last transmitted DeviceSelection. Used to prevent sending redundant information */
+	private boolean[] lastSelection;
 	
 	//Properties of the Hardware
-	public final int NUM_FADERS;
-	public final int NUM_FADERS_PER_BANK;
-	public final int NUM_BFADERS;
-	public final int NUM_XFADERS;
-	public final int NUM_ROTARYS;
-	public final int NUM_DEVICES;
+	/** Number of normal faders at Controller */			public final int NUM_FADERS;
+	/** Number of faders in one bank at Controller */		public final int NUM_FADERS_PER_BANK;
+	/** Number of multiplexed faders at Controller */		public final int NUM_BFADERS;
+	/** Number of special purpose faders at Controller */	public final int NUM_XFADERS;
+	/** Number of rotary encoders at Controller */			public final int NUM_ROTARYS;
+	/** Number of selectable devices at Controller */		public final int NUM_DEVICES;
 	
-	public final int OFFSET_FADERS;
-	public final int OFFSET_ROTARY;
-	public final int OFFSET_X;
-	public final int OFFSET_MULTIPLEX;
+	/** First analog channel used for normal faders */		public final int OFFSET_FADERS;
+	/** First analog channel used for rotary encoders */	public final int OFFSET_ROTARY;
+	/** First analog channel used for special faders */		public final int OFFSET_X;
+	/** First analog channel used for multiplexed faders*/	public final int OFFSET_MULTIPLEX;
+	/** First Channel of DeviceTransmission */				public final int FIRST_DEVICE_TRANSMISSION;
+	/** First Channel of DeviceSelection */					public final int FIRST_DEVICE_SELECTION;
+	/** Channel of DMX-Status-LED */						public final int DMX_STATUS_LED;
+	/** First Channel of multiplex information pins */		public final int OFFSET_MULTIPLEX_PINS;
 	
-	public final int FADER_TOLERANCE;
+	/** Fader movement, which is interpreted as noise */	public final int FADER_TOLERANCE;
 	
-	public final String ARDUINO_PORT;
+	/** Port, where the Arduino is connected */				public final String ARDUINO_PORT;
 	
 	/**
 	 * Returns list of active COM-Ports
@@ -64,21 +74,25 @@ public class WingController {
 	 */
 	public WingController(Properties prop) {
 		//Read Properties
-		NUM_FADERS			= Integer.parseInt(prop.getProperty("NUM_FADERS", "16"));
-		NUM_FADERS_PER_BANK	= Integer.parseInt(prop.getProperty("NUM_FADERS_PER_BANK", "8"));
-		NUM_BFADERS			= Integer.parseInt(prop.getProperty("NUM_BFADERS", "8"));
-		NUM_XFADERS			= Integer.parseInt(prop.getProperty("NUM_XFADERS", "4"));
-		NUM_ROTARYS			= Integer.parseInt(prop.getProperty("NUM_ROTARYS", "3"));
-		NUM_DEVICES			= Integer.parseInt(prop.getProperty("NUM_DEVICES", "30"));
+		NUM_FADERS					= Integer.parseInt(prop.getProperty("NUM_FADERS", "16"));
+		NUM_FADERS_PER_BANK			= Integer.parseInt(prop.getProperty("NUM_FADERS_PER_BANK", "8"));
+		NUM_BFADERS					= Integer.parseInt(prop.getProperty("NUM_BFADERS", "8"));
+		NUM_XFADERS					= Integer.parseInt(prop.getProperty("NUM_XFADERS", "4"));
+		NUM_ROTARYS					= Integer.parseInt(prop.getProperty("NUM_ROTARYS", "3"));
+		NUM_DEVICES					= Integer.parseInt(prop.getProperty("NUM_DEVICES", "30"));
 
-		OFFSET_FADERS		= Integer.parseInt(prop.getProperty("OFFSET_FADERS", "0"));
-		OFFSET_ROTARY		= Integer.parseInt(prop.getProperty("OFFSET_ROTARY", "13"));
-		OFFSET_X			= Integer.parseInt(prop.getProperty("OFFSET_X", "8"));
-		OFFSET_MULTIPLEX	= Integer.parseInt(prop.getProperty("OFFSET_MULTIPLEX", "12"));
+		OFFSET_FADERS				= Integer.parseInt(prop.getProperty("OFFSET_FADERS", "0"));
+		OFFSET_ROTARY				= Integer.parseInt(prop.getProperty("OFFSET_ROTARY", "13"));
+		OFFSET_X					= Integer.parseInt(prop.getProperty("OFFSET_X", "8"));
+		OFFSET_MULTIPLEX			= Integer.parseInt(prop.getProperty("OFFSET_MULTIPLEX", "12"));
+		FIRST_DEVICE_TRANSMISSION	= Integer.parseInt(prop.getProperty("FIRST_DEVICE_TRANSMISSION", "16"));
+		FIRST_DEVICE_SELECTION		= Integer.parseInt(prop.getProperty("FIRST_DEVICE_SELECTION", "64"));
+		DMX_STATUS_LED				= Integer.parseInt(prop.getProperty("DMX_STATUS_LED", "56"));
+		OFFSET_MULTIPLEX_PINS		= Integer.parseInt(prop.getProperty("OFFSET_MULTIPLEX_PINS", "1"));
 		
-		FADER_TOLERANCE		= Integer.parseInt(prop.getProperty("FADER_TOLERANCE", "0"));
+		FADER_TOLERANCE				= Integer.parseInt(prop.getProperty("FADER_TOLERANCE", "0"));
 		
-		ARDUINO_PORT		= prop.getProperty("ARDUINO_PORT");
+		ARDUINO_PORT				= prop.getProperty("ARDUINO_PORT");
 		
 		
 		this.faders = new int[Constants.DEVICE_CHANNELS];
@@ -147,7 +161,7 @@ public class WingController {
 	public boolean[] checkActivity(boolean use) {
 		boolean state;
 		for (int i = 0; i < NUM_DEVICES; i++) {
-			state = (arduino.digitalRead(i + ProFirmata.FIRST_DEVICE_TRANSMISSION) == Arduino.HIGH);
+			state = (arduino.digitalRead(i + FIRST_DEVICE_TRANSMISSION) == Arduino.HIGH);
 			activityChanged[i] = (state != lastState[i]);
 			if (use) lastState[i] = state;
 		} /* for */
@@ -166,7 +180,7 @@ public class WingController {
 	public void setActiveDevices(boolean[] isActive, boolean force) {
 		for (int i = 0; i < NUM_DEVICES; i++) {
 			if (force || (lastSelection[i] != isActive[i])) {
-				arduino.digitalWrite(i + ProFirmata.FIRST_DEVICE_SELECTION, (isActive[i]) ? Arduino.HIGH : Arduino.LOW);
+				arduino.digitalWrite(i + FIRST_DEVICE_SELECTION, (isActive[i]) ? Arduino.HIGH : Arduino.LOW);
 				lastSelection[i] = isActive[i];
 			} /* if */
 		} /* for */
@@ -183,12 +197,12 @@ public class WingController {
 		}
 		
 		//Read Multiplexed
-		int m0 = arduino.digitalRead(1);
-		int m1 = arduino.digitalRead(2);
-		int m2 = arduino.digitalRead(3);
+		int m0 = arduino.digitalRead(OFFSET_MULTIPLEX_PINS);
+		int m1 = arduino.digitalRead(OFFSET_MULTIPLEX_PINS + 1);
+		int m2 = arduino.digitalRead(OFFSET_MULTIPLEX_PINS + 2);
 		bFaders[m0 + 2*m1 + 4*m2] = arduino.analogRead(OFFSET_MULTIPLEX);
 	}
 	public void setStatusLED(boolean value) {
-		if (value) arduino.digitalWrite(56, Arduino.HIGH); else arduino.digitalWrite(56, Arduino.LOW);
+		if (value) arduino.digitalWrite(DMX_STATUS_LED, Arduino.HIGH); else arduino.digitalWrite(DMX_STATUS_LED, Arduino.LOW);
 	}
 }
