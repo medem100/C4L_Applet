8fa52c8076ff6dccd1d3bdc4b6510dbd375122a5
Andre Soltau <Modellbauer555@web.de>
2019-03-15 16:24:27 +0100

    add serverAvalibale

--- src/c4l/applet/main/Util.java
+++ src/c4l/applet/main/Util.java
@@ -1,7 +1,17 @@
 package c4l.applet.main;
 
-public final class Util {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import org.apache.log4j.Logger;
+
+public final class Util {
+	private static Logger Log = Logger.getRootLogger();
+	/**
+	 * Return the status of the Program
+	 * @return
+	 */
 	public static Boolean getTestRun() {
 		if (!(System.getProperty("TestRun") == null)) {
 			return Boolean.valueOf(System.getProperty("TestRun"));
@@ -9,6 +19,26 @@ public final class Util {
 			return false; // it isn´t a Test Run When the the Property isn´t set
 		}
 
+	}
+	
+	public static Boolean getServerAvalibal() {
+		try {
+		URL url = new URL("http://example.com");
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.connect();
+
+		 if(connection.getResponseCode() == 200 ) {
+			 return true;
+		 }else {
+			 return false;
+		 }
+		
+		}catch (IOException e) {
+			Log.error("Fail to check Server avalibale -> wrong path", e);
+			return false;
+		}
+		
 	}
 	/*
 	 * public static Boolean getServerAvailable() { //
