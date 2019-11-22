105f8835ebfc7251f9f4b6c40c81f5e6673332dc
Andre Soltau <Modellbauer555@web.de>
2019-11-22 15:19:03 +0100

    Insert static scene

--- src/c4l/applet/db/Insert.java
+++ src/c4l/applet/db/Insert.java
@@ -52,33 +52,57 @@ public class Insert {
 	//
 	// }
 
-	public ResultSet scene(Device[] Devices, int setupID) {
+	/**
+	 * scene Scene to setup
+	 * 
+	 * @param Devices
+	 * @param setupID
+	 * @return
+	 * @throws Exception 
+	 */
+	public Integer scene(Device[] Devices, int setupID, String name, String description) throws Exception {
 		logger.debug("insert new scene");
 
 		try {
 			conn = dbCreate.getInstance();
-			String findSQL = "select d.device_id from device d " + "inner join setup_has_device shd "
+			String SELECT_DEVICE_ID = "select d.device_id from device d " + "inner join setup_has_device shd "
 					+ "on shd.device_id = d.device_id " + "where shd.setup_id =" + setupID + " and d.start_address =";
 
-			String INSERT_RECORD = "insert into device_status(input, device_id) values(?,?)";
+			String INSERT_DEVICE_STATUS = "insert into device_status(input, device_id) values(?,?)";
 
-			PreparedStatement pstmt = conn.prepareStatement(INSERT_RECORD,Statement.RETURN_GENERATED_KEYS);
-			
+			PreparedStatement insertDevceStatusStatment = conn.prepareStatement(INSERT_DEVICE_STATUS,
+					Statement.RETURN_GENERATED_KEYS);
+
 			// insert the device statis
 			for (Device device : Devices) {
 
-				int deviceID = Integer.valueOf(select.getOneData(findSQL + device.getStartAddres() + ";", "device_id"));
-				logger.debug("deviceId: " + deviceID);
-				pstmt.setString(1, toSaveString(device.getInputs()));
-				pstmt.setInt(2, deviceID);
-				pstmt.addBatch();
+				String dId = select.getOneData(SELECT_DEVICE_ID + device.getStartAddres() + ";", "device_id");
+				if (dId != null) {
+					int deviceID = Integer.valueOf(dId);
+					logger.debug("deviceId: " + deviceID);
+					insertDevceStatusStatment.setString(1, toSaveString(device.getInputs()));
+					insertDevceStatusStatment.setInt(2, deviceID);
+					insertDevceStatusStatment.addBatch();
+				}else {
+					throw new Exception("Device not Found: sid:"+ setupID+ " addres: "+device.getStartAddres());
+				}
 
 			}
-			
+
 			// Execut the batchs
-			  int[] updateCounts = pstmt.executeBatch();
-			  return pstmt.getGeneratedKeys();
-			  
+			int[] updateCounts = insertDevceStatusStatment.executeBatch();
+			ResultSet deviceStatusIds = insertDevceStatusStatment.getGeneratedKeys();
+
+			// Insert new Scene
+
+			int sceneID = createScene(name, description);
+			addSceneToSetUp(setupID, sceneID);
+
+			// add Device status to scene
+			addDeviceStatusToScene(deviceStatusIds, sceneID);
+
+			return sceneID;
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -88,7 +112,89 @@ public class Insert {
 		}
 	}
 
-	public ResultSet device(int[] permutation, String description, String name, int[] rotaryChannels, int startAddress,
+	public void addDeviceStatusToScene(ResultSet deviceStatusIds, int sceneID) {
+		logger.debug("add device status to scene :" + sceneID);
+		String INSERT_DS_TO_SCENE = "insert into scene_has_device_status(scene_id,device_status_id) values(?,?)";
+		try {
+			conn = dbCreate.getInstance();
+			PreparedStatement insertNewSceneStatment = conn.prepareStatement(INSERT_DS_TO_SCENE);
+			while (deviceStatusIds.next()) {
+				insertNewSceneStatment.setInt(1, sceneID);
+				insertNewSceneStatment.setInt(2, deviceStatusIds.getInt(1));
+				insertNewSceneStatment.addBatch();
+			}
+			insertNewSceneStatment.executeBatch();
+
+		} catch (SQLException e) {
+			logger.error(e);
+		}
+	}
+
+	/**
+	 * crate new scene entry returns the new id
+	 * 
+	 * @param name
+	 * @param description
+	 * @return id of the new scene
+	 */
+	private Integer createScene(String name, String description) {
+		logger.debug("create new scene");
+		String INSERT_SCENE = "insert into scene(scene_name,scene_description) values(?,?)";
+		try {
+			conn = dbCreate.getInstance();
+			PreparedStatement insertNewSceneStatment = conn.prepareStatement(INSERT_SCENE,
+					Statement.RETURN_GENERATED_KEYS);
+
+			insertNewSceneStatment.setString(1, name);
+			insertNewSceneStatment.setString(2, description);
+			insertNewSceneStatment.execute();
+
+			ResultSet rs = insertNewSceneStatment.getGeneratedKeys();
+			rs.next();
+			return rs.getInt(1);
+
+		} catch (SQLException e) {
+			logger.error(e);
+			return null;
+		}
+
+	}
+
+	/**
+	 * add new Scene to a setup
+	 * 
+	 * @param setUpID
+	 * @param sceneID
+	 */
+	private void addSceneToSetUp(int setUpID, int sceneID) {
+		logger.debug("scene: " + sceneID + " to setup: " + setUpID);
+		String INSERT_SCENE_TO_SETUP = "insert into setup_has_scene(setUp_id,scene_id) values(?,?)";
+		try {
+			conn = dbCreate.getInstance();
+			PreparedStatement insertNewSceneStatment = conn.prepareStatement(INSERT_SCENE_TO_SETUP);
+			insertNewSceneStatment.setInt(1, setUpID);
+			insertNewSceneStatment.setInt(2, sceneID);
+			insertNewSceneStatment.execute();
+		} catch (SQLException e) {
+			logger.error(e);
+		}
+	}
+
+	/**
+	 * insert new device
+	 * 
+	 * @param permutation
+	 * @param description
+	 * @param name
+	 * @param rotaryChannels
+	 * @param startAddress
+	 * @param virtualDimmerChannel
+	 * @param virtualDimming
+	 * @param deviceCategory
+	 * @param mainEffectChanels
+	 * @return new Device id
+	 */
+	public Integer device(int[] permutation, String description, String name, int[] rotaryChannels, int startAddress,
 			int virtualDimmerChannel, int[] virtualDimming, int deviceCategory, int[] mainEffectChanels) {
 		logger.debug("insert device");
 		try {
@@ -109,7 +215,9 @@ public class Insert {
 
 			preparedStmt.execute();
 
-			return preparedStmt.getGeneratedKeys();
+			ResultSet rs = preparedStmt.getGeneratedKeys();
+			rs.next();
+			return rs.getInt(1);
 
 		} catch (SQLException e) {
 			logger.error("can´t insert device", e);
